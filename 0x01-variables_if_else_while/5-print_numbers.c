f26b07b283fa119206d09e1d2c4b16a3e73a1a96
luca <alaahusain1999@gmail.com>
2023-04-22 01:55:20 +0000

    add file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints didigt from 0 to 9
+ * Return: Alwas 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+		printf("%i", digit);
+	printf("\n");
+	return (0);
+}
