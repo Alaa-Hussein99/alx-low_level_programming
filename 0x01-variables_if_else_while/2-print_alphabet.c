ae270e66af4a2c56a76ec342abc8d2da7b94cfdd
luca <alaahusain1999@gmail.com>
2023-04-22 01:26:39 +0000

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints characters from a to z
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch >= 'z')
+		putchar(ch);
+		putchar('\n');
+		ch++;
+}
