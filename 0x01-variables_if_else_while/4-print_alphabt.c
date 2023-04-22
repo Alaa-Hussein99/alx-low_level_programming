e342fc795e85c1cd010a7330a020d27700439788
luca <alaahusain1999@gmail.com>
2023-04-22 01:51:02 +0000

    add file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all characters in lower case except q and e
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
