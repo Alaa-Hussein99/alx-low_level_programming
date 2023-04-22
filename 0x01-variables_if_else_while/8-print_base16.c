bd9a1723e0a609682a446bc1d8c3488a8d904bc4
luca <alaahusain1999@gmail.com>
2023-04-22 02:19:58 +0000

    add file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints numbers of base 16
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit = 0;
+	char ch = 'a';
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0')'
+		digit++;
+	}
+	while (ch <= 'f')
+	{
+		putcahr(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
