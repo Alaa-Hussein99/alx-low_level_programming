e04c07cb10f02a6eebfa37bf51fb2b373ed016c3
luca <alaahusain1999@gmail.com>
2023-04-22 02:57:19 +0000

    add file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints combination
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int digit = 9;
+	int i, j;
+
+	for (i = 0; i < digit; i++)
+	{
+		for (j = i + 1; j <= digit; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	putchar('\n');
+	return (0);
+}
