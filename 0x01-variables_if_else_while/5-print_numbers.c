57a044a9d16e684082918142c5c09ccbb273bda2
luca <alaahusain1999@gmail.com>
2023-04-22 02:11:28 +0000

    add file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,7 +9,10 @@ int main(void)
 	int digit = 0;
 
 	while (digit <= 9)
+	{
 		printf("%i", digit);
+		digit++;
+	}
 	printf("\n");
 	return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,7 +9,10 @@ int main(void)
 	int digit = 0;
 
 	while (digit <= 9)
+	{
 		putchar(digit + '0');
+		digit++;
+	}
 	putchar('\n');
 	return (0);
 }
