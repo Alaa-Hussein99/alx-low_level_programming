2bc77c02c7a7f809468c91a778918a5408bc413a
luca <alaahusain1999@gmail.com>
2023-04-27 00:18:29 +0000

    add files

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - function that checks if the character is in lower case or not
+ * @c: the character that will be checked
+ * Return: 1 if lower case, 0 otherwise
+*/
+int _islower(int c)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (c == i)
+			return (1);
+		else
+			return (0)
+	}
+}
