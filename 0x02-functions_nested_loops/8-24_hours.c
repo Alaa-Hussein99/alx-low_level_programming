e50eac1f30eaf7ee266b83425a44b56c77e90b03
luca <alaahusain1999@gmail.com>
2023-04-27 01:30:22 +0000

    add files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * starting from 00:00 to 23:59
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				printf("0");
+			printf("%d", i);
+			_putchar(':');
+			if (j < 10)
+				printf("0");
+			printf("%d", j);
+			_putchar('\n');
+		}
+	}
+}
