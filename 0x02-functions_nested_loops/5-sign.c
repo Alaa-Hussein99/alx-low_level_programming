787c4f8e27e6ce086a1e65cf901bea66e046d6ab
luca <alaahusain1999@gmail.com>
2023-04-27 00:38:29 +0000

    add files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0, ");
+		printf(48);
 		return (0);
 	}
 	else
 	{
-		printf("-, ");
+		printf('-');
 		return (-1);
 	}
 }
