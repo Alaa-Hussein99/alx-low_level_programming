0d683fa1fb05254ff48caef5744e45172bf07bea
luca <alaahusain1999@gmail.com>
2023-04-27 07:46:29 +0000

    add file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints the sum of the even numbers in fibonacci
+ * Return: Always 0
+*/
+int main(void)
+{
+	long int t1 = 0;
+	long int t2 = 1;
+	long int next = t1 + t2;
+	long int sum = 0;
+
+	while (1)
+	{
+		if (next > 4000000)
+			break;
+		if ((next % 2) == 0)
+		{
+			sum = sum + next
+		}
+		f1 = f2;
+		f2 = next;
+		next = f1 + f2;
+	}
+	printf("ld\n", sum);
+	return (0);
+}
