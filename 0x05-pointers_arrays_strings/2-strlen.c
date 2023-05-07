5025b286fe546c81f530fb127e063af9af16a96e
luca <alaahusain1999@gmail.com>
2023-05-07 19:10:56 +0000

    add

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - takes a string and counts its characters
+ * @s: the i/p string
+ * Return: return the number of characters in the string
+*/
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++
+	return (n);
+}
