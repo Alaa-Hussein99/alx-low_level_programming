7e5fbd884356028f092360ce5b6754e8aa739e68
luca <alaahusain1999@gmail.com>
2023-05-08 16:45:44 +0000

    strcpy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	while (src[i] != '\0')
 		i++;
 	for (n = 0; n < i; n++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest[i]);
+		dest[n] = src[n];
+	dest[n] = '\0';
+	return (dest);
 }
