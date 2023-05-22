280794d58cfda519f740e5f5df638208bdc6c0be
luca <alaahusain1999@gmail.com>
2023-05-22 03:32:55 +0000

    add i++

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	l = 0;
 	while (dest[l])
 		l++;
-	for (i = 0; i < n && src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[l + i] = src[i];
 	dest[l + i] = '\0';
 	return (dest);
