0cedbe1347f6feea50f5fa1f8ac6faaa24c85a7e
luca <alaahusain1999@gmail.com>
2023-05-25 02:29:50 +0000

    add files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,7 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+		printf("Error\n"), exit(98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
@@ -20,13 +19,11 @@ int main(int argc, char **argv)
 	op_f = get_op_func(argv[2]);
 	if (!op_f)
 	{
-		printf("Error\n");
-		exit(99);
+		printf("Error\n"), exit(99);
 	}
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		printf("Error\n");
-		exit(100);
+		printf("Error\n"), exit(100);
 	}
 	printf("%d\n", op_f(a, b));
 	return (0);
