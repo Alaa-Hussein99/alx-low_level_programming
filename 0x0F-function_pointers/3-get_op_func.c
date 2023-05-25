10cfc1e554ecb43ef35e4b18a68b29ee85fefd29
luca <alaahusain1999@gmail.com>
2023-05-25 02:23:45 +0000

    add files

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "calc.h"
+/**
+ * get_op_func - returns pointer to the required fun
+ * @s: operator string
+ * Return: the required function pointer
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (s && s[0] == ops[i].op[0] && !s[1])
+			return (ops[i].f);
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "calc.h"
+/**
+ * op_add - adds two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the addition
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the subtraction
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplicates two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the multiplication
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the division
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
