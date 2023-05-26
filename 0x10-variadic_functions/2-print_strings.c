ad4d8bda607cf0a2ea834e78645c40d204815e4d
luca <alaahusain1999@gmail.com>
2023-05-26 22:27:57 +0000

    separator*

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(arg, char *);
 		printf("%s", str != NULL ? str : "(nil)");
-		if (i < (n - 1) && seperator != NULL)
+		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
