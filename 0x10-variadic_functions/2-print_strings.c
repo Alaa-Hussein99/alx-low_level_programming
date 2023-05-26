c1605feae071e5f3bf415a5ba41c49e6d64e7450
luca <alaahusain1999@gmail.com>
2023-05-26 22:20:22 +0000

    argument of type char *

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", (str = va_arg(arg, int)) != NULL ? str : "(nil)");
+		printf("%s", (str = va_arg(arg, char *)) != NULL ? str : "(nil)");
 		if (i < (n - 1) && seperator != NULL)
 			printf("%s", separator);
 	}
