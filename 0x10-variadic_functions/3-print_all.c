0507ab2667907b3befc9342a98b92f8de0970b69
luca <alaahusain1999@gmail.com>
2023-05-26 23:11:32 +0000

    add print all file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "variadic_functions.c"
+/**
+ * form_char - format char
+ * @separator: given separator
+ * @arg: argument pointer
+*/
+void form_char(char *separator, va_list arg)
+{
+	printf("%s%c", separator, va_arg(arg, int));
+}
+
+/**
+ * form_int - format integer
+ * @separator: given separator
+ * @arg: argument pointer
+*/
+void form_int(char *separator, va_list arg)
+{
+	printf("%s%d", separator, va_arg(arg, int));
+}
+
+/**
+ * form_float - format float
+ * @separator: given separator
+ * @arg: argument pointer
+*/
+void form_float(char *separator, va_list arg)
+{
+	printf("%s%f", separator, va_arg(arg, double));
+}
+
+/**
+ * form_str - format string
+ * @separator: given separator
+ * @arg: argument pointer
+*/
+void form_str(char *separator, va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+	printf("%s%s", separator, str);
+}
+
+/**
+ * print_all - prints anything in any format
+ * @format: the format used to print
+ * @...: given input
+*/
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	int j;
+	char *separator = "";
+	va_list arg;
+	token_t tokens[] = {
+		{"c", form_char},
+		{"i", form_int},
+		{"f", form_float},
+		{"s", form_str},
+		{NULL, NULL}
+	};
+
+	va_start(arg, format);
+	while (format && format(i))
+	{
+		j = 0;
+		while (tokens[j].token)
+		{
+			if (format[i] == tokens[j].token[0])
+			{
+				tokens[j].f(separator, arg);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/***
+ * struct token - struct
+ * @token: format
+ * @f: the pointer to function
+*/
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
