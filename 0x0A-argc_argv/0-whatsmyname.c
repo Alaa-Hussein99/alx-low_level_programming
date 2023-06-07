31904e589747fc837d28dddbe79201924f980182
luca <alaahusain1999@gmail.com>
2023-06-07 18:22:25 +0000

    add

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entryy point
+ * @argc: int
+ * @argv: int
+ * Return: Always 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
